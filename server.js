18840cb67563ffd9cb603c14584197fac533b191
KevsonDoc <kevsonfilipesantos@gmail.com>
2020-09-03 20:52:38 -0300

    [add] - adding landing page

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const server = express();
+
+server.get('/', function(req, res) {
+    res.sendFile(__dirname + '/static/index.html')
+});
+
+server.use('/css', express.static(__dirname + '/static/css'));
+server.use('/javascript', express.static(__dirname + '/static/javascript'));
+
+server.use('/err', express.static(__dirname + '/static/err/index.html'));
+
+server.use((req, res, next) => {
+    res.status(404).redirect('/err')
+});
+
+server.listen(process.env.PORT || 5000, function() {
+    console.log('Running done!')
+});
